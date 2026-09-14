3547e258855ee288fb99b567cc46793d36748614
Mippcoding <mippyu22@gmail.com>
2026-09-14 13:34:22 +0700

    menambahkan suhu di gudang

--- models/warehouse.php
+++ models/warehouse.php
@@ -1,12 +1,32 @@
 <?php
+
 // models/warehouse.php
 
 function warehouse_all($pdo) {
-    return $pdo->query("SELECT * FROM warehouses ORDER BY name ASC")->fetchAll();
+
+    return $pdo->query("
+        SELECT * 
+        FROM warehouses 
+        ORDER BY name ASC
+    ")->fetchAll();
+
 }
 
 function warehouse_create($pdo, $data) {
-    $stmt = $pdo->prepare("INSERT INTO warehouses (name, location) VALUES (?, ?)");
-    $stmt->execute([$data['name'], $data['location']]);
+
+    $stmt = $pdo->prepare("
+        INSERT INTO warehouses 
+        (name, location, min_temperature, max_temperature) 
+        VALUES (?, ?, ?, ?)
+    ");
+
+    $stmt->execute([
+        $data['name'],
+        $data['location'],
+        $data['min_temperature'],
+        $data['max_temperature']
+    ]);
+
     return $pdo->lastInsertId();
+
 }
