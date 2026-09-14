ec49c53f0e873e34bb2dfc898c10240958b5cef9
Mippcoding <mippyu22@gmail.com>
2026-09-14 13:10:29 +0700

    mengembalikan code yang hilang

--- models/warehouse.php
+++ models/warehouse.php
@@ -5,12 +5,6 @@ function warehouse_all($pdo) {
     return $pdo->query("SELECT * FROM warehouses ORDER BY name ASC")->fetchAll();
 }
 
-function warehouse_get($pdo, $id) {
-    $stmt = $pdo->prepare("SELECT * FROM warehouses WHERE id = ?");
-    $stmt->execute([$id]);
-    return $stmt->fetch();
-}
-
 function warehouse_create($pdo, $data) {
     $stmt = $pdo->prepare("INSERT INTO warehouses (name, location) VALUES (?, ?)");
     $stmt->execute([$data['name'], $data['location']]);
