f076b7702f9cece677176922dcedc39b8ee2e4c6
hesaa920 <dsalmanan014@gmail.com>
2026-09-15 11:10:55 +0700

    mengupdate stock php

--- stock.php
+++ stock.php
@@ -8,20 +8,20 @@ require_once 'models/inventory.php';
 require_login();
 
 $warehouseId = isset($_GET['warehouse_id']) ? (int)$_GET['warehouse_id'] : null;
+$search = isset($_GET['search']) ? trim($_GET['search']) : '';
 
 // Populate data for filters
 $warehouses = warehouse_all($pdo);
 
 // Fetch stock based on filter
 if ($warehouseId) {
-    $stockItems = inventory_get_warehouse_stock($pdo, $warehouseId);
+    $stockItems = inventory_get_warehouse_stock($pdo, $warehouseId, $search);
 } else {
-    // Flatten global overview logic slightly for the detailed list
-    $stockItems = inventory_get_global_overview($pdo);
+    $stockItems = inventory_get_global_overview($pdo, $search);
 }
 
 $title = "Data Stok";
 
 require 'views/layout/header.php';
 require 'views/inventory/stock_list.php';
-require 'views/layout/footer.php';
+require 'views/layout/footer.php';';
